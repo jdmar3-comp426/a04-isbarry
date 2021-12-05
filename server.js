c038c25cfe0626e3aed81d7711810c1a5a4d5364
Ibrahim S Barry <isbarry@Ibrahims-MacBook-Pro.local>
2021-12-04 22:23:34 -0600

    laka

--- server.js
+++ server.js
@@ -27,8 +27,8 @@ app.get("/app/", (req, res, next) => {
 // CREATE a new user (HTTP method POST) at endpoint /app/new/
 app.post("/app/new", (req, res) => {
 	const stmt = db.prepare("INSERT INTO userinfo (user,pass) VALUES (?,?)");
-const info = stmt.run(req.body.user, req.body.pass);
-res.status(201).send({message: info.changes + " record created: ID " + info.lastInsertRowid});
+	const info = stmt.run(req.body.user, req.body.pass);
+	res.status(201).send({message: info.changes + " record created: ID " + info.lastInsertRowid});
 
 });
 
@@ -47,7 +47,7 @@ app.get("/app/users/:id", (req, res) => {
 
 
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
-app.patch("/app/users/:id", (req, res) => {	
+app.patch("/app/update/user/:id", (req, res) => {	
 	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id = ?");
 	const info = stmt.run(req.body.user, req.body.pass);
 	res.status(200).send(info.changes+ " record updated: ID " +info.lastInsertRowid);
